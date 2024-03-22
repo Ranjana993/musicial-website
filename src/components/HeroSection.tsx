e9bc4e15d4a97eceb9ef9606816ac2f5f1c8b909
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-22 22:09:47 +0530

    improved ui hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,12 +5,12 @@ import { Button } from "./ui/moving-border";
 
 export function HeroSection() {
   return (
-    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center mt-[22rem] lg:mt-0 bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center py-[2rem] lg:py-0 bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-24 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Spotlight <br /> is the new trend.
         </h1>

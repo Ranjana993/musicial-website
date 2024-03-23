f618eda1226f23a19574a71e7d2ee732eaa56fba
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-23 22:04:56 +0530

    improved ui

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,16 +5,16 @@ import { Button } from "./ui/moving-border";
 
 export function HeroSection() {
   return (
-    <div className="h-[60rem] w-full rounded-md flex md:items-center md:justify-center py-[2rem] lg:py-0 bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="h-[52rem] w-full rounded-md flex md:items-center md:justify-center py-[2rem] lg:py-0 bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Spotlight
         className="-top-60 left-0 md:left-60 md:-top-20"
         fill="white"
       />
       <div className="p-12 max-w-7xl  mx-auto relative z-10  w-full pt-24 md:pt-0">
-        <h1 className="text-4xl mt-12  md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+        <h1 className="text-5xl mt-12  md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Spotlight <br /> is the new trend.
         </h1>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+        <p className="mt-6 font-normal text-md  text-neutral-300 max-w-lg text-center mx-auto">
           Spotlight effect is a great way to draw attention to a specific part
           of the page. Here, we are drawing the attention towards the text
           section of the page. I don&apos;t know why but I&apos;m running out of
@@ -23,7 +23,7 @@ export function HeroSection() {
         <div className="mt-12 flex justify-center items-center ">
           <Link href={"/courses"}>
             <Button
-              borderRadius="1.75rem"
+              borderRadius="1.5rem"
               className="bg-white w-full dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800 "
             >
               Explore courses

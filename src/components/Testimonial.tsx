2f63927193e82f4715e9beedffb9df118cb06c6f
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-27 14:18:00 +0530

    working on upcoming webinar

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -5,14 +5,24 @@ import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export function Testimonial() {
   return (
-    <div className="h-[40rem] rounded-md flex flex-col antialiased bg-gray-500 dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <h2 className="text-4xl lg:text-6xl py-2 mb-12 text-center">What People say about Us </h2>
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
-    </div>
+    <>
+      <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <h2 className="text-4xl font-bold lg:text-6xl py-2 mb-12 text-center">What People say about Us </h2>
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
+      <div className="rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="left"
+          speed="slow"
+        />
+      </div>
+    </>
+
   );
 }
 
---
+++ src/components/UpcomingWbinar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpcomingWbinar = () => {
+  return (
+    <div>UpcomingWbinar</div>
+  )
+}
+
+export default UpcomingWbinar

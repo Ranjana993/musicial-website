44f10b37f6bf33b9065644c8f75a59161aa03d71
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-03-28 12:49:08 +0530

    Ui improved of cards

--- src/components/UpcomingCards.tsx
+++ src/components/UpcomingCards.tsx
@@ -187,7 +187,7 @@ const SkeletonFour = () => {
     >
       <motion.div
         variants={first}
-        className="h-full w-1/3 rounded-2xl bg-black p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+        className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
       >
         <Image
           src={logo}
